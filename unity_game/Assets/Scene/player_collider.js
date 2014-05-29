26ff54b24e283e8b16d3b4ed5f9f7fecd43f6203
ipvr2014 <ipvr2000@gmail.com>
2014-05-29 23:32:06 +0800

    Finish two scenes
    1.  完成兩個場景
    2.  "player_collider.js "
    若碰到目的地(ex. 郵局...)，會以Text方式顯示在螢幕。
    請掛在使用者(膠囊)身上

---
+++ unity_game/Assets/Scene/player_collider.js
@@ -0,0 +1,56 @@
+﻿#pragma strict
+var seven_11:boolean;
+var postOffice:boolean;
+var mcdonald: boolean;
+
+function Start () 
+{
+	seven_11 = false;
+	postOffice = false;
+}
+
+function Update () 
+{
+
+}
+
+function OnTriggerEnter(collision : Collider)
+{
+	if(collision.name=="7-11")
+	{
+		seven_11 = true;
+		Debug.Log("7-11 true");
+	}
+	else if(collision.name=="postoffice")
+	{
+		postOffice = true;
+		Debug.Log("post true");
+	}
+	else if(collision.name=="mcdonald")
+	{
+		mcdonald = true;
+		Debug.Log("m true");
+	}
+		
+		
+	
+	
+}
+
+function OnGUI()
+{
+	if(seven_11==false)
+	GUILayout.Label(" 7-11 not found");
+	else if(seven_11==true)
+	GUILayout.Label(" 7-11 is found");
+	
+	if(postOffice==false)
+	GUILayout.Label(" post office not found");
+	else if(postOffice==true)
+	GUILayout.Label(" post Office is found");
+	
+	if(mcdonald==false)
+	GUILayout.Label(" mcdonald not found");
+	else if(mcdonald==true)
+	GUILayout.Label(" mcdonald is found");
+}
